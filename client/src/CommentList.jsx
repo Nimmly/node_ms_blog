131275654e9b042c46f60434b0670b64f8d9aab2
Marko Popovic <markopopovic@Markos-MacBook-Pro.local>
2022-11-23 11:52:23 +0100

    initial state of the project

---
+++ client/src/CommentList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const CommentList = ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+  return <ul>{renderedComments}</ul>;
+};
